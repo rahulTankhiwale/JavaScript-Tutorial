67ffa8b2f731f9937bb7180ce624deb1e67b9963
rahulTankhiwalez <rahultankhiwale14@gmail.com>
2024-07-12 11:10:46 +0530

    concept of data_types

---
+++ basics/data_types.js
@@ -0,0 +1,39 @@
+/* 
+JavaScript has 8 Datatypes
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+
+The object data type can contain both built-in objects, and user defined objects:
+Built-in object types can be:
+objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
+*/ 
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "tankhiwale";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = { firstname :"Rahul" ,lastName: "Tankhiwale"}
+
+// Array object:
+const arrays = ["bat", "ball", "gloves"]
+
+// Date object:
+const date = new Date("2022-03-25");
+
+console.log(typeof undefined ); // undefined
+console.log(typeof null); // object
